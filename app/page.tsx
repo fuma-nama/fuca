5e52e725ee14da0b7e2a3d19d83ff063f90dee2e
Fuma Nama <god63820869@gmail.com>
2025-02-24 12:23:48 +0800

    support pixel ratio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/cn";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
@@ -33,10 +34,10 @@ export default function Home() {
       />
       <label
         htmlFor="upload"
-        className="px-6 py-2.5 bg-black text-white/80 font-medium text-sm mt-4 cursor-pointer rounded-full"
+        className="px-6 py-2.5 bg-white text-black  /80 font-medium text-sm mt-6 cursor-pointer rounded-full"
         style={{
           boxShadow:
-            "inset 6px 6px 8px rgba(255, 255, 255, 0.4), inset 6px 2px 8px rgba(255, 255, 255, 0.6), inset -3px -2px 4px rgba(255, 255, 255, 0.6), inset -2px -6px 12px rgba(0, 0, 0, 0.6)",
+            "inset 6px 6px 8px rgba(25, 25, 25, 0.2), inset 0px 2px 8px rgba(55, 55, 55, 0.6), inset -3px -2px 4px rgba(255, 255, 255, 0.2), inset -2px -6px 12px rgba(0, 0, 0, 0.6)",
         }}
       >
         Upload
@@ -50,6 +51,7 @@ function Stone({ imageUrl }: { imageUrl: string }) {
   const svgRef = useRef<SVGSVGElement>(null);
   const [vector, setVector] = useState([0, 0]);
   const [useBackground, setUseBackground] = useState(true);
+  const [size, setSize] = useState(1000);
 
   useEffect(() => {
     const onMove = (event: MouseEvent) => {
@@ -61,103 +63,129 @@ function Stone({ imageUrl }: { imageUrl: string }) {
     };
 
     window.addEventListener("mousemove", onMove);
+
+    setSize(
+      window.devicePixelRatio * Math.min(1000, document.body.clientWidth)
+    );
     return () => window.removeEventListener("mousemove", onMove);
   }, []);
 
   return (
-    <svg
-      ref={svgRef}
-      width="1000"
-      height="1000"
-      className="max-w-screen"
-      onClick={() => {
-        setUseBackground((prev) => !prev);
-      }}
-    >
-      <defs>
-        <filter id="spotlight">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.1"
-            numOctaves="4"
-            result="noise"
-          />
-          <feColorMatrix
-            in="SourceGraphic"
-            type="matrix"
-            values="0 0 0 0 0
+    <>
+      <svg
+        ref={svgRef}
+        width={size}
+        height={size}
+        className="w-[1000px] h-auto max-w-full aspect-square"
+      >
+        <defs>
+          <filter id="spotlight">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.1"
+              numOctaves="4"
+              result="noise"
+            />
+            <feColorMatrix
+              in="SourceGraphic"
+              type="matrix"
+              values="0 0 0 0 0
               0 0 0 0 0
               0 0 0 0 0
               1 1 1 0.2 0"
-            result="alpha_map"
-          />
+              result="alpha_map"
+            />
 
-          <feComposite
-            in="alpha_map"
-            in2="noise"
-            operator="arithmetic"
-            result="alpha_map_with_noise"
-            k2="1"
-            k3="-0.2"
-          />
-          <feDiffuseLighting
-            in="alpha_map_with_noise"
-            result="light"
-            lightingColor="rgba(250,250,250,1)"
-            diffuseConstant={1}
-          >
-            <fePointLight x={vector[0]} y={vector[1]} z="30" />
-          </feDiffuseLighting>
+            <feComposite
+              in="alpha_map"
+              in2="noise"
+              operator="arithmetic"
+              result="alpha_map_with_noise"
+              k2="1"
+              k3="-0.2"
+            />
+            <feDiffuseLighting
+              in="alpha_map_with_noise"
+              result="light"
+              lightingColor="rgba(250,250,250,1)"
+              diffuseConstant={1}
+            >
+              <fePointLight x={vector[0]} y={vector[1]} z="32" />
+            </feDiffuseLighting>
 
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.1"
-            numOctaves="4"
-            result="turbulence"
-          />
-          <feDisplacementMap
-            in2="turbulence"
-            in="SourceGraphic"
-            result="source_with_displacement"
-            scale="50"
-            xChannelSelector="R"
-            yChannelSelector="G"
-          />
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.1"
+              numOctaves="4"
+              result="turbulence"
+            />
+            <feDisplacementMap
+              in2="turbulence"
+              in="SourceGraphic"
+              result="source_with_displacement"
+              scale="50"
+              xChannelSelector="R"
+              yChannelSelector="G"
+            />
 
-          <feComposite
-            in="SourceGraphic"
-            in2="light"
-            operator="arithmetic"
-            result="out1"
-            k1="1"
-          />
+            <feComposite
+              in="SourceGraphic"
+              in2="light"
+              operator="arithmetic"
+              result="out1"
+              k1="1"
+            />
 
-          <feDropShadow
-            dx={(-vector[0] * 12) / 1000}
-            dy={(-vector[1] * 12) / 1000}
-            stdDeviation="15"
-            floodColor="black"
-            floodOpacity="1"
-          />
-        </filter>
-      </defs>
+            <feDropShadow
+              dx={(-vector[0] * 14) / 1000}
+              dy={(-vector[1] * 14) / 1000}
+              stdDeviation="20"
+              floodColor="black"
+              floodOpacity="1"
+            />
+          </filter>
+        </defs>
 
-      <g filter="url(#spotlight)">
-        {useBackground && (
-          <rect
-            x="10%"
-            y="10%"
-            width="80%"
-            height="80%"
-            rx="20"
-            stroke="0"
-            style={{
-              fill: "gray",
-            }}
+        <g filter="url(#spotlight)">
+          {useBackground && (
+            <rect
+              x="10%"
+              y="10%"
+              width="80%"
+              height="80%"
+              rx="20"
+              stroke="0"
+              style={{
+                fill: "gray",
+              }}
+            />
+          )}
+          <image href={imageUrl} x="10%" y="10%" width="80%" height="80%" />
+        </g>
+      </svg>
+      <div
+        className="sticky bottom-2 p-2 rounded-full bg-neutral-800 text-sm"
+        style={{
+          boxShadow:
+            "inset 6px 6px 8px rgba(25, 25, 25, 0.2), inset 0px 2px 8px rgba(55, 55, 55, 0.6), inset -3px -2px 4px rgba(255, 255, 255, 0.2), inset -2px -6px 12px rgba(0, 0, 0, 0.6)",
+        }}
+      >
+        <button
+          onClick={() => setUseBackground((prev) => !prev)}
+          className={cn(
+            "relative cursor-pointer text-neutral-400 px-4 py-2.5 rounded-full font-medium transition-colors",
+            useBackground && "text-white"
+          )}
+        >
+          Background
+          <div
+            className={cn(
+              "absolute inset-0 bg-white/20 blur-sm rounded-full transition-opacity z-[-1]",
+              !useBackground && "opacity-0"
+            )}
           />
-        )}
-        <image href={imageUrl} x="10%" y="10%" width="80%" height="80%" />
-      </g>
-    </svg>
+        </button>
+      </div>
+    </>
   );
 }
